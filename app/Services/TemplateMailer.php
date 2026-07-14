53f053be4a5dc8c766d5c82aaf156e382f3a902e
James <techsolutioncamp@gmail.com>
2026-07-14 17:51:16 +0100

    Commit all pending changes

--- app/Services/TemplateMailer.php
+++ app/Services/TemplateMailer.php
@@ -3,12 +3,15 @@
 namespace App\Services;
 
 use App\Mail\AdminEmailTemplate;
+use App\Models\PortalSetting;
 use Illuminate\Support\Facades\Mail;
 
 class TemplateMailer
 {
     public static function send(string $recipientEmail, string $slug, array $variables, string $defaultSubject, string $defaultHtml, ?string $defaultText = null, ?string $name = null, ?string $category = null): void
     {
+        $variables = static::normalizeVariables($variables);
+
         [$subject, $html, $text] = static::render($slug, $variables, $defaultSubject, $defaultHtml, $defaultText, $name, $category);
 
         try {
@@ -34,5 +37,27 @@ class TemplateMailer
         );
 
         return [$rendered['subject'], $rendered['html'], $rendered['text']];
+    }
+
+    protected static function normalizeVariables(array $variables): array
+    {
+        if (! array_key_exists('logo', $variables)) {
+            $logoPath = PortalSetting::where('key', 'logo_path')->value('value');
+            if (! empty($logoPath)) {
+                $variables['logo'] = asset('storage/' . ltrim($logoPath, '/'));
+            } else {
+                $variables['logo'] = 'https://via.placeholder.com/160x90.png?text=AHHC+Logo';
+            }
+        }
+
+        if (! array_key_exists('organization', $variables)) {
+            $variables['organization'] = config('app.name', 'AHHC Portal');
+        }
+
+        if (! array_key_exists('year', $variables)) {
+            $variables['year'] = now()->year;
+        }
+
+        return $variables;
     }
 }
